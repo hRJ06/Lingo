d0cc0b85b158e585e72875c90128af5238d8538f
hRJ06 <hindolroy.2306@gmail.com>
2024-07-08 22:45:17 +0530

    Added Marketing skeleton

---
+++ app/(marketing)/layout.tsx
@@ -0,0 +1,19 @@
+import { Footer } from "@/components/Footer";
+import { Header } from "@/components/Header";
+
+type Props = {
+  children: React.ReactNode;
+};
+const MarketingLayout = ({ children }: Props) => {
+  return (
+    <div className="min-h-screen flex flex-col">
+      <Header />
+      <main className="flex-1 flex flex-col items-center justify-center">
+        {children}
+      </main>
+      <Footer />
+    </div>
+  );
+};
+
+export default MarketingLayout;
---
+++ components/Header.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const Header = () => {
+  return (
+    <header className='h-20 w-full border-b-2 border-slate-200 px-4'>
+        Header
+    </header>
+  )
+}

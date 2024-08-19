5ca6dee79620bf81500cfd0dd41e421c94ea689f
hRJ06 <hindolroy.2306@gmail.com>
2024-08-20 00:48:57 +0530

    Deleted Cloudfare configuration

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,9 +1,4 @@
 /** @type {import('postcss-load-config').Config} */
-import {setupDevPlatform} from '@cloudflare/next-on-pages/next-dev';
-
-if (process.env.NODE_ENV === 'development') {
-    await setupDevPlatform();
-}
 const config = {
     plugins: {
         tailwindcss: {},
